06680098696a80ecc1437a6f3350fa253b206fe7
aveigel <alexaveigel@yahoo.com>
2019-05-16 13:49:18 -0600

    Stub boot project and connect to JPA

---
+++ BeerQ/src/main/java/com/skilldistillery/beerlab/BeerQApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beerlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerQApplication.class, args);
+	}
+
+}
