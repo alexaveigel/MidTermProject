ac30e33f0f79f7997b7c1289172f54eecacd7c17
aveigel <alexaveigel@yahoo.com>
2019-05-17 11:58:39 -0600

    Tests for bar owner, brewery, fav beer

--- BeerLabJPA/src/test/java/com/skilldistillery/beerlab/entities/BarOwnerTest.java
+++ BeerLabJPA/src/test/java/com/skilldistillery/beerlab/entities/BarOwnerTest.java
@@ -1,6 +1,6 @@
 package com.skilldistillery.beerlab.entities;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,10 +12,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class DrinkerTest {
+class BarOwnerTest {
 	private static EntityManagerFactory emf;
 	private EntityManager em;
-	private Drinker drinker;
+	private BarOwner barOwner;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -30,25 +30,19 @@ class DrinkerTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		em = emf.createEntityManager();
-		drinker = em.find(Drinker.class, 1);
+		barOwner = em.find(BarOwner.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		em.close();
-		drinker = null;
+		barOwner = null;
 	}
 
 	@Test
-	void test_drinker_mapping() {
-		assertEquals(1, drinker.getUserId());
-		assertEquals("The", drinker.getFirstName());
-		assertEquals("Drinker", drinker.getLastName());
-		assertEquals("1995-01-30", drinker.getDob().toString());
-		assertEquals("Girl", drinker.getGender());
-		assertEquals("IPA", drinker.getBeerStyle());
-		assertEquals("pic.com", drinker.getPicUrl());
-		assertEquals(1, drinker.getAddressId());
+	void test_brewery_mapping() {
+		assertEquals(1, barOwner.getBarId());
+		assertEquals(1, barOwner.getUserId());
 	}
 
 }
