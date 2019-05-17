44785db9d02c82ca0f0b5f42076f1070cd9efef2
alfarog1 <alfaro.geovanny@gmail.com>
2019-05-17 08:55:05 -0600

    added user and drinker entities

---
+++ BeerLabJPA/src/main/java/com/skilldistillery/beerlab/entities/Drinker.java
@@ -0,0 +1,174 @@
+package com.skilldistillery.beerlab.entities;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+public class Drinker {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "user_id")
+	private String userId;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "date_of_birth")
+	@Temporal(TemporalType.DATE)
+	private Date dob;
+	private String gender;
+	@Column(name = "beer_style")
+	private String beerStyle;
+	@Column( name = "pic_url")
+	private String picUrl;
+	@Column(name = "address_id")
+	private int addressId;
+	public Drinker() {
+		super();
+	}
+	public Drinker(int id, String userId, String firstName, String lastName, Date dob, String gender, String beerStyle,
+			String picUrl, int addressId) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.gender = gender;
+		this.beerStyle = beerStyle;
+		this.picUrl = picUrl;
+		this.addressId = addressId;
+	}
+	@Override
+	public String toString() {
+		return "Drinker [id=" + id + ", userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", dob=" + dob + ", gender=" + gender + ", beerStyle=" + beerStyle + ", picUrl=" + picUrl
+				+ ", addressId=" + addressId + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + addressId;
+		result = prime * result + ((beerStyle == null) ? 0 : beerStyle.hashCode());
+		result = prime * result + ((dob == null) ? 0 : dob.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((picUrl == null) ? 0 : picUrl.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Drinker other = (Drinker) obj;
+		if (addressId != other.addressId)
+			return false;
+		if (beerStyle == null) {
+			if (other.beerStyle != null)
+				return false;
+		} else if (!beerStyle.equals(other.beerStyle))
+			return false;
+		if (dob == null) {
+			if (other.dob != null)
+				return false;
+		} else if (!dob.equals(other.dob))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (picUrl == null) {
+			if (other.picUrl != null)
+				return false;
+		} else if (!picUrl.equals(other.picUrl))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		return true;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBeerStyle() {
+		return beerStyle;
+	}
+	public void setBeerStyle(String beerStyle) {
+		this.beerStyle = beerStyle;
+	}
+	public String getPicUrl() {
+		return picUrl;
+	}
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+}
