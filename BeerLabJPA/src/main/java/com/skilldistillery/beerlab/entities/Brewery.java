1a79f24d4ca30113fbab377fdafeba83da685a3e
Michael Quesada <michaelquesada@SDMacBookPro3-2.local>
2019-05-17 14:28:36 -0600

    Mapped Brewery and Bar to Address and Test

--- BeerLabJPA/src/main/java/com/skilldistillery/beerlab/entities/Brewery.java
+++ BeerLabJPA/src/main/java/com/skilldistillery/beerlab/entities/Brewery.java
@@ -1,10 +1,13 @@
 package com.skilldistillery.beerlab.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Brewery {
@@ -18,11 +21,24 @@ public class Brewery {
 	@Column(name = "logo_url")
 	private String logoUrl;
 	
-	@Column(name = "address_id")
-	private int addressId;
-	
 	@Column(name = "website_url")
 	private String webUrl;
+	
+	// We may not need the Cascade Persist
+	// One to One unidirectional with Address
+	// Deleted regular field called addressId so this field wouldn't conflict
+	@OneToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name= "address_id")
+	private Address address;
+	
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
 	public int getId() {
 		return id;
@@ -48,14 +64,6 @@ public class Brewery {
 		this.logoUrl = logoUrl;
 	}
 
-	public int getAddressId() {
-		return addressId;
-	}
-
-	public void setAddressId(int addressId) {
-		this.addressId = addressId;
-	}
-
 	public String getWebUrl() {
 		return webUrl;
 	}
@@ -63,31 +71,32 @@ public class Brewery {
 	public void setWebUrl(String webUrl) {
 		this.webUrl = webUrl;
 	}
+	
 
 	@Override
 	public String toString() {
-		return "Brewery [id=" + id + ", name=" + name + ", logoUrl=" + logoUrl + ", addressId=" + addressId
-				+ ", webUrl=" + webUrl + "]";
-	}
-
-	public Brewery(int id, String name, String logoUrl, int addressId, String webUrl) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.logoUrl = logoUrl;
-		this.addressId = addressId;
-		this.webUrl = webUrl;
+		return "Brewery [id=" + id + ", name=" + name + ", logoUrl=" + logoUrl + ", webUrl=" + webUrl + ", address="
+				+ address + "]";
 	}
 
 	public Brewery() {
 		super();
 	}
 
+	public Brewery(int id, String name, String logoUrl, String webUrl, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.logoUrl = logoUrl;
+		this.webUrl = webUrl;
+		this.address = address;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + addressId;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
 		result = prime * result + id;
 		result = prime * result + ((logoUrl == null) ? 0 : logoUrl.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
@@ -95,7 +104,6 @@ public class Brewery {
 		return result;
 	}
 
-	//Override
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -105,7 +113,10 @@ public class Brewery {
 		if (getClass() != obj.getClass())
 			return false;
 		Brewery other = (Brewery) obj;
-		if (addressId != other.addressId)
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
 			return false;
 		if (id != other.id)
 			return false;
@@ -127,6 +138,10 @@ public class Brewery {
 		return true;
 	}
 	
+	
+
+
+
 	
 	
 	
